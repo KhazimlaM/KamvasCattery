77f17363e029e32d0279d7996798159e20a56890
Khazimla <20113497@rcconnect.co.za>
2022-04-22 16:28:18 +0200

    assessment

--- bookings_cost/test/bookings.tests.js
+++ bookings_cost/test/bookings.tests.js
@@ -1,1 +1,33 @@
+describe('Test for my bookingCost function' , function(){
+    it("it should return 'R135' if the function is called with 'R45 * 3' " , function(){
+        assert.equal(135,bookingCost({
+            check_in_day: 'Monday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }));
 
+    });
+
+    it("it should return 'R114.75' if the function is called with 'R45 * 3 * 0.85 ' " , function(){
+        assert.equal(114.75,bookingCost({
+            check_in_day: 'Friday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }));
+
+    });
+
+    it("it should return 'R101.25' if the function is called with 'R45 * 3 * 0.75' " , function(){
+        assert.equal(101.25,bookingCost({
+            check_in_day: 'Wednesday',
+            duration: 3,
+            cat_name : 'Snowy'
+          }));
+
+    });
+    
+
+});
+
+
+    
--- weekly_wages/weekly_wages.js
+++ weekly_wages/weekly_wages.js
@@ -1,3 +1,13 @@
 function weeklyWage(band, weeklyHours) {
-
+ var bands = band.split(",")
+ var hours = weeklyHours.split(",")
+ 
+  for(var i=0; i<weeklyHours.length; ++i){
+     if( weeklyHours < 7){
+     return "missing days"
+     }
+     else if( weeklyHours === 7){
+    return weeklyHours.length
+     }
+  }
 };
